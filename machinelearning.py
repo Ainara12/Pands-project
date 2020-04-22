178e34fe3ae93d06bf4b8968ffbb915262ce4ae2
Ainara Ruiz <you@example.com>
2020-04-22 14:18:38 +0100

    Added machine learning code last version

--- machinelearning.py
+++ machinelearning.py
@@ -1,22 +1,20 @@
 
-#I created this file to try to predict which is the iris flower species by taking into account its features mesaurements
+#I created this file to try to predict which is the iris flower species by taking into account its features measurements
 
-#I first import sklearn library
+#I first import sklearn library which has stored our iris dataset and has the functionalities to make this work
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split#I imported the model from library
 from sklearn.neighbors import KNeighborsClassifier#This model is going to allow us to use K nearest neighbors algorithm 
-import numpy as np 
-
-#import matplotlib.pyplot as plt 
+import numpy as np #I also import numpy for our arrays of values
+import matplotlib.pyplot as plt #And I import matplotlib for the scatter plots
 
 iris= load_iris()
 
-#print( iris.keys())#Whis this option I am showing the keys stored as dictionary 
+print( iris.keys()) #Whis this option I am showing the keys stored as dictionary in this libary
 
-#print(iris)
 
-#Then in order to separate the 4 features ( sepal length, sepal width, petal length and petal width )we create variables for each of them
+#Then in order to separate the 4 features ( sepal length, sepal width, petal length and petal width )I create variables for each of them
 
 features = iris.data.T
 
@@ -30,26 +28,29 @@ sepal_width_label =iris.feature_names[1]
 petal_length_label = iris.feature_names[2]
 petal_width_label = iris.feature_names[3]
 
-#Scatter plot 1 creation:
+#In order to display the scatter plots, run command plt.show()
+#Scatter plot 1:
 
-#plt.scatter(sepal_length, sepal_width, c=iris.target)
-#plt.xlabel(sepal_length_label)
-#plt.ylabel(sepal_width_label)
-#plt.title("Sepal length & sepal width correlation")
-#plt.show()
+plt.scatter(sepal_length, sepal_width, c=iris.target)
+plt.xlabel(sepal_length_label)
+plt.ylabel(sepal_width_label)
+plt.title("Sepal length & sepal width correlation")
+plt.show()
 
-#Scatter plot 2 creation:
+#Scatter plot 2 :
 
-#plt.scatter(petal_length, petal_width, c=iris.target)
-#plt.xlabel(petal_length_label)
-#plt.ylabel(petal_width_label)
-#plt.title("Petal length & petal width correlation")
-#plt.legend()
-#plt.show()
+plt.scatter(petal_length, petal_width, c=iris.target)
+plt.xlabel(petal_length_label)
+plt.ylabel(petal_width_label)
+plt.title("Petal length & petal width correlation")
+plt.show()
 
-#In order to split the data into 25% ( for test)and 75% we use the following
+#Prediction model: 
+
+#The first step is to split the data.
+# In order to split the data into 25% ( for test)and 75% we use the following
 #functionality that sklearn provides:
-#here we select data as we want to split the data and also target as we want
+#here we select data as we want to split data to perform tests and also target to
 #predict which species the flower belongs to ( specified as target in this library).
 
 X_train, X_test, y_train, y_test = train_test_split(iris["data"],iris["target"])
@@ -58,15 +59,15 @@ knn= KNeighborsClassifier(n_neighbors=1) #we created variable for Kneighbors cla
 knn.fit(X_train, y_train)#Then we add to our fit function training data and training data from our target
 
 #Now we are ready to run this model, lets enter a sample for a new specimen and 
-#see in which species will fir according to this model:
+#see in which species it will fit according to this model:
 
-X_new = np.array([[0.3,1.0,3.5,6.0]]) #enter an array of float values for each flower part
+X_new = np.array([[5.0,3.0,0.2,0.3]]) #enter an array of float values for each flower part
 
 #To predict considering this array we define a variable for the prediction and
 #use function predict: 
 
-#prediction = knn.predict(X_new)
-#print( prediction) 
+prediction = knn.predict(X_new)
+print( prediction) 
 
 #The prediction function returns one of the three values 
 #  assigned to each of the three iris species:
@@ -75,4 +76,10 @@ X_new = np.array([[0.3,1.0,3.5,6.0]]) #enter an array of float values for each f
 #[1]for versicolor
 #[2]for virginica
 
+#I also added option to test the accuracy of the predictions, I use knn.score function an indicate
+# to use the portion of data that we kept for testing. 
+
 print(knn.score(X_test, y_test))
+
+print ('Thanks for using my first attemp of machine learning !')
+
